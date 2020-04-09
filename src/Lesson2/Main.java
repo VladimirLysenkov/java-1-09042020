3ed6db2d057852a3190fad42a4ffa04d4256bbd1
vladimir_lysenkov <vladimirv.lysenkov@gmail.com>
2020-04-09 23:26:18 +0500

    создал package Lesson1 и перенес в него класс с 1 урока создал package Lesson2 и класс для 2 домашнего задания

---
+++ src/Lesson2/Main.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args){
+        for (byte i = 100; i != 0; i++) {
+            System.out.println(i + "");
+        }
+
+        int y = 256;
+
+        while (y > 0){
+            System.out.println(y);
+            y/=2;
+        }
+
+
+    }
+
+
+}
