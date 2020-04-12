aa6debf4de62b4619d1b0e24ea46037f5a3a4763
vladimir_lysenkov <vladimirv.lysenkov@gmail.com>
2020-04-12 22:57:42 +0500

    1 задание

--- src/Lesson2/Main.java
+++ src/Lesson2/Main.java
@@ -1,20 +1,27 @@
 package Lesson2;
 
+
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args){
-        for (byte i = 100; i != 0; i++) {
-            System.out.println(i + "");
-        }
 
-        int y = 256;
-
-        while (y > 0){
-            System.out.println(y);
-            y/=2;
-        }
-
-
+        int[] array1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        invertArray(array1);
+        System.out.println("Инвертированный массив: " + Arrays.toString(array1));
     }
+
+    //Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    private static void invertArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (arr[i] == 1) ? 0 : 1;
+        }
+    }
+
+
+
+
 
 
 }
